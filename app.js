6c84c7fad9447d97ace6c1cde8eb1ae39bf0e75b
Nathan Heck <nathan.heck@gmail.com>
2020-04-22 14:54:07 -0600

    Basic structure complete. The code works but needs comparisons

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
+let rock = document.getElementById("rock");
+    paper = document.getElementById("paper");
+    scissors = document.getElementById("scissors");
+
+let arr = ["rock", "paper", "scissors"];
+
+let playerArr = [rock, paper, scissors];
+
 function computerPlay(){
-    let arr=["rock", "paper", "scissors"];
-    
+    let num = Math.floor(Math.random()*3);
+    computerSelection = arr[num];
+    console.log(computerSelection);
+    return computerSelection;
 };
+
+function playerPlay(choice){
+    let playerSelection = choice.id;
+    console.log(playerSelection);
+    return playerSelection;
+};
+
+for (let choice of playerArr){
+choice.addEventListener('click', function(){
+    playerPlay(choice);
+    computerPlay();
+});
+};
+
+/*function playRound(playerSelection, computerSelection){
+    console.log("you chose rock")
+};*/
